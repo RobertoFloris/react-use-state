fe7aff5d4d4eb6fe5725baf8c921842ed1938845
RobertoFloris <florisroberto99@gmail.com>
2025-01-16 18:09:19 +0100

    [add] no language selected

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -4,7 +4,7 @@ const Main = (props) => {
 
   const { languages } = props;
 
-  const [selectedId, setSelectedId] = useState(1)
+  const [selectedId, setSelectedId] = useState(0)
 
   return (
     <main>
@@ -15,16 +15,21 @@ const Main = (props) => {
           <button key={language.id} type="button" className={selectedId == language.id ? "btn btn-warning m-3" : "btn btn-primary m-3"} onClick={() => setSelectedId(language.id)} >{language.title}</button>
         ))}
 
-        <div class="card">
-          <div class="card-body">
+        <div className="card">
+          <div className="card-body">
 
-            <div>
-              <h2>{languages.find(language => language.id == selectedId).title}</h2>
-            </div>
+            {selectedId == 0 ? <p>Nessun linguaggio selezionato</p> :
+              <div>
 
-            <div>
-              <p>{languages.find(language => language.id == selectedId).description}</p>
-            </div>
+                <div>
+                  <h2>{languages.find(language => language.id == selectedId).title}</h2>
+                </div>
+
+                <div>
+                  <p>{languages.find(language => language.id == selectedId).description}</p>
+                </div>
+
+              </div>}
 
           </div>
         </div>
