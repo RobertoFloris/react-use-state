e4c423d2bfaa875f6d6d115797e720d49449260a
RobertoFloris <florisroberto99@gmail.com>
2025-01-16 16:51:19 +0100

    [add] card

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -2,11 +2,29 @@ const Main = (props) => {
   const { languages } = props;
   return (
     <main>
+
       <div className="container">
+
         {languages.map(language => (
           <button key={language.id} type="button" className="btn btn-primary m-3">{language.title}</button>
         ))}
+
+        <div class="card">
+          <div class="card-body">
+
+            <div>
+              <h2>{languages[0].title}</h2>
+            </div>
+
+            <div>
+              {languages[0].description}
+            </div>
+
+          </div>
+        </div>
+
       </div>
+
     </main>
   )
 }
